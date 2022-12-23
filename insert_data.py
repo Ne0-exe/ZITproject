c8b622e5df1eb26271ebd7f2e417ca3b6b543377
Ne0-exe <ne0exe@protonmail.com>
2022-12-23 11:40:12 +0100

    Final update

--- insert_data.py
+++ insert_data.py
@@ -5,6 +5,7 @@ import sys
 ipL = analyse.ipList
 sortDict = {ipL[0]:1}
 sortDict = {i:ipL.count(i) for i in ipL}
+dang_ips = {}
 
 with open('ips.txt', 'w') as sys.stdout:
     sD = sortDict
@@ -21,7 +22,7 @@ with open('ips.txt', 'w') as sys.stdout:
     cursor.execute(command1)
 
     ranksL = [5, 30]
-
+    f = open('dang_ips.txt', 'w')
     for x, y in sD.items():
         if y < ranksL[0]:
             cursor.execute("INSERT INTO ip_db (IPs, Counts , Rank) VALUES(?, ?, ?)", (x, y, 'safe'))
@@ -33,13 +34,17 @@ with open('ips.txt', 'w') as sys.stdout:
             continue
         else:
             cursor.execute("INSERT INTO ip_db (IPs, Counts , Rank) VALUES(?, ?, ?)", (x, y, 'dangerous'))
+            dang_ips[x] = y
             conn.commit()
+
+    for key, value in dang_ips.items():
+        f.write('%s appeared %s times\n' % (key, value))
 
     sql = "SELECT * FROM ip_db ORDER BY Counts ASC"
     cursor.execute(sql)
     rows = cursor.fetchall()
-    for row in rows:
-        print(row)
+
 
     conn.close()
+
 
